c445b661dc99a2e89388e38043b54a687c7fa2db
Eray Rafet <erayrafet@gmail.com>
2025-03-05 13:57:58 +0200

    Correct device names and screen resolution

--- device.mk
+++ device.mk
@@ -21,3 +21,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+    
+# Screen
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2408
